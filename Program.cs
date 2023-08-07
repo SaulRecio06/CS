b2bc191f293f5c4dbf427cb58a48fde2803aee1a
SaulRecio06 <76199088+SaulRecio06@users.noreply.github.com>
2023-08-06 22:53:09 -0700

    Database connection now using .env

--- Program.cs
+++ Program.cs
@@ -4,13 +4,21 @@ global using CSApi.Dtos.Character;
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore;
 global using CSApi.Data;
+global using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+DotNetEnv.Env.Load();
+
+
+
+var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
+var DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
+
+var DbConnection = $"Server={DB_HOST}; Database={DB_NAME}; Trusted_Connection=true;TrustServerCertificate=true;";
 
 // Add services to the container.
-
-
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(DbConnection));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +27,14 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<INterCharacter, CharacterService>();
 
 var app = builder.Build();
+
+
+
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
